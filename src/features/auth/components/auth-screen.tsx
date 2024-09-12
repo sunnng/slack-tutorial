83bf85615e23fbf0c630413aad331864927b617e
Scy <chengyuan7766@gmail.com>
2024-09-12 16:49:23 +0800

    set up sign in.up card

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
+import { SignInFlow } from "../type";
+
 export const AuthScreen = () => {
-  return <div>Auth screen</div>;
+  const [state, setState] = useState<SignInFlow>("signIn");
+
+  return (
+    <div className="h-full flex items-center justify-center bg-[#5C3B58]">
+      <div className="md:h-auto md:w-[420px]">Auth screen</div>
+    </div>
+  );
 };
